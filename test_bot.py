7a531943ea84bb666a64fecd4d6e977a92e2f643
JugaadBhai <93419355+JugaadBhai@users.noreply.github.com>
2023-01-26 13:55:43 +0530

    Add files via upload

---
+++ test_bot.py
@@ -0,0 +1,11 @@
+import aiml
+import time
+
+time.clock = time.time
+
+kernel = aiml.Kernel()
+kernel.learn("std-startup.xml")
+kernel.respond("LOAD")
+
+while True:
+  print(kernel.respond(input("Enter your message >> ")))
